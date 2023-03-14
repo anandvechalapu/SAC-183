948352912b880dcee1c82f09592185f5b99a3534
anandvechalapu <anand.vechalapu@gmail.com>
2023-03-14 09:36:25 +0530

    Updated

--- controller/GithubActivationController.java
+++ controller/GithubActivationController.java
@@ -1,30 +1,40 @@
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 @RestController
-@RequestMapping("/github")
+@RequestMapping("/github-activation")
 public class GithubActivationController {
  
-    @PostMapping("/activation")
-    public ResponseEntity<Object> activateGithub(@RequestBody GithubCredentials githubCredentials) {
-       
-        // Validate the Github credentials
-        boolean isValidCredentials = validateGithubCredentials(githubCredentials);
-       
-        if(isValidCredentials) {
+    @RequestMapping("/connect")
+    public String connectGithub(@RequestParam("username") String username, @RequestParam("password") String password, RedirectAttributes redirectAttributes) {
+        // Validate Github credentials
+        String message = "Invalid credentials";
+        if (isValidCredentials(username, password)) {
             // Connect to Github
-            List<GithubRepository> repositories = connectToGithub(githubCredentials);
-           
-            // Return the list of repositories
-            return ResponseEntity.ok().body(repositories);
+            message = "Connected to Github successfully";
         } else {
-            // Return an error message
-            return ResponseEntity.badRequest().body("Invalid Github credentials");
+            redirectAttributes.addFlashAttribute("message", message);
         }
+        return message;
     }
  
-    private boolean validateGithubCredentials(GithubCredentials githubCredentials) {
-        // Validate the Github credentials
+    @RequestMapping("/list")
+    public List<String> listGithubRepositories() {
+        // Get list of Github repositories
+        List<String> repositories = getGithubRepositories();
+        return repositories;
     }
-   
-    private List<GithubRepository> connectToGithub(GithubCredentials githubCredentials) {
-        // Connect to Github
+ 
+    private boolean isValidCredentials(String username, String password) {
+        // Validate Github credentials
+        return true;
+    }
+ 
+    private List<String> getGithubRepositories() {
+        // Get list of Github repositories
+        List<String> repositories = new ArrayList<>();
+        return repositories;
     }
 }
